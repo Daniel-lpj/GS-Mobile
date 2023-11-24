c489900c7b0194cbcd7dc76e75a8f5ec3aa287f3
Daniel Junior <dlpjunior@apoioprodesp.sp.gov.br>
2023-11-24 16:19:39 -0300

    Base URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.11:8080/api",
+  baseURL: "https://api-gsolution-digital.azurewebsites.net/api",
 });
 
 export default api;
